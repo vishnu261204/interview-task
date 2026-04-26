d97b30fb8929b8d319583f83118cc8797f5891ab
vishnu261204 <rvvishnuram26@gmail.com>
2026-04-26 16:21:43 +0530

    update

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const errorMiddleware = require('./middleware/errorMiddleware');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
